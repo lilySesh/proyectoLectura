61f8264699131f71d1cd811bcf4fd56f83d7d505
lilySesh <liliana.cochancela.est@tecazuay.edu.ec>
2023-07-04 17:39:10 -0500

    subiendo

---
+++ src/app/header/header.component.spec.ts
@@ -0,0 +1,7 @@
+import { HeaderComponent } from './header.component';
+
+describe('HeaderComponent', () => {
+  it('should create an instance', () => {
+    expect(new HeaderComponent()).toBeTruthy();
+  });
+});
