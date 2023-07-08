c215c55f5bd5fd524c0d4fd8f1d7689e6c5ea741
lilySesh <liliana.cochancela.est@tecazuay.edu.ec>
2023-07-07 19:12:23 -0500

    pantalla de inicio

--- src/app/header/header.component.spec.ts
+++
@@ -1,7 +0,0 @@
-import { HeaderComponent } from './header.component';
-
-describe('HeaderComponent', () => {
-  it('should create an instance', () => {
-    expect(new HeaderComponent()).toBeTruthy();
-  });
-});
